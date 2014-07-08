e7d5bfc5c4ed1e0ad083c3efc4370f90240b7060
Emrah Ede <emrahede@yahoo.ca>
2014-07-08 08:09:39 -0400

    require plugin file added

--- inc/init.php
+++ inc/init.php
@@ -2,7 +2,7 @@
 defined('ABSPATH') or die("No script kiddies please!");
 
 /* Sets the path to the parent theme directory URI. */
-if ( !defined( 'CB_THEME_URI' ) ) {
+if ( !defined( 'WBS_THEME_URI' ) ) {
 	define( 'WBS_THEME_URI', get_stylesheet_directory_uri() );
 }
 
@@ -10,3 +10,4 @@ if(!defined('LANGUAGE_ZONE'))
 	define('LANGUAGE_ZONE', 'websolns');
 
 include 'setup_theme.php';
+include 'require_plugins.php';
