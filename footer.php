2a672f7296f99f56e63613c367b6fc596e7e5643
Emrah Ede <emrahede@yahoo.ca>
2015-01-31 14:07:55 -0500

    analytic added footer

--- footer.php
+++ footer.php
@@ -44,6 +44,16 @@ defined('ABSPATH') or die("No script kiddies please!");
   }
 </script>
 <?php wp_footer(); ?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o)
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-56286358-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </body>
 
 </html>
